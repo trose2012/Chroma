b91ccd1acee90e95e11ea75ee632221772eb64f9
Theodore Rose <111089339+trose2012@users.noreply.github.com>
2025-02-25 08:10:15 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://Chr0ma.me/",
   base: "/",
   trailingSlash: "always",
   integrations: [
